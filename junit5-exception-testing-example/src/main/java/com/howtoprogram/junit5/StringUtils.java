e9dc162b231f579039769d17cc62f8cac022297f
Chester Sim <chestersimdy@gmail.com>
2019-05-28 14:34:19 +0800

    Revert "Bitches"

--- junit5-exception-testing-example/src/main/java/com/howtoprogram/junit5/StringUtils.java
+++ junit5-exception-testing-example/src/main/java/com/howtoprogram/junit5/StringUtils.java
@@ -6,10 +6,7 @@ public final class StringUtils {
     if (str == null || str.trim().length() == 0) {
       throw new IllegalArgumentException("String must be not null or empty");
     }
-    System.out.println("HELLO");
-    String[] a = "HELLO".split("");
-    
-    return null;
+    return Integer.valueOf(str);
   }
 }
 
