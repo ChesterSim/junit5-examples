f549753ad487cc8429786b0f6fccd19674ad2de9
Andy Lam <andyylam1997@gmail.com>
2019-05-28 13:53:50 +0800

    Remove first package

--- junit5-exception-testing-example/src/main/java/com/howtoprogram/junit5/StringUtils.java
+++ junit5-exception-testing-example/src/main/java/com/howtoprogram/junit5/StringUtils.java
@@ -6,7 +6,7 @@ public final class StringUtils {
     if (str == null || str.trim().length() == 0) {
       throw new IllegalArgumentException("String must be not null or empty");
     }
-    return Integer.valueOf(str);
+    return null;
   }
 }
 
